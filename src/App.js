55cc96965d951073e35d496bf5e0ac63eb43bd54
mikefab <mikefabrikant@gmail.com>
2018-05-20 19:31:14 -0400

    Debug deployment

--- src/App.js
+++ src/App.js
@@ -58,7 +58,8 @@ class App extends Component {
     });
 
     this.setState({map});
-
+    console.log('!!!!')
+    console.log(apiConfig);
     // Promises
     let shapesPromise  = fetch(apiConfig.shapes).then((response) => response.json())
     let schoolsPromise = fetch(apiConfig.schools).then((response) => response.json())
