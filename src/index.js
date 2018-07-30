00168ee6976f04a38de7dd6df1c66b78515a31b7
mikefab <mikefabrikant@gmail.com>
2018-07-30 10:24:43 -0400

    start auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './authorization/Home/Home';
+import Callback from './authorization/Callback/Callback';
+import Auth from './authorization/Auth/Auth';
+import history from './authorization/history';
+import Shield from './authorization/Shield';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
