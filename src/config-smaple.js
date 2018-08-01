6f011601cbdc879b9b5e071b8944bd1bd0d038bd
Vinit Singh <vinitveersingh@gmail.com>
2018-08-01 11:09:11 -0400

    Removed Typo in config sample file

--- src/config-smaple.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  auth_client_id: process.env.REACT_APP_AUTH_CLIENT_ID || 'APP_AUTH_CLIENT_ID',
-  auth_callback_url: process.env.REACT_APP_AUTH_CALLBACK_URL || 'http://localhost:3000/authorization/callback',
-  login_required: process.env.REACT_APP_LOGIN_REQUIRED || true
-}
