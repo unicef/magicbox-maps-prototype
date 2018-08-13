f0844f3a1077f749a339a7f6a2a8ae85c5c57978
Thoa T <thoat@users.noreply.github.com>
2018-08-13 15:48:41 -0400

    Clean up configuration files

--- src/config-sample.js
+++ src/config-sample.js
@@ -1,13 +1,29 @@
+// Colombia
 module.exports = {
-  auth_client_id: process.env.REACT_APP_AUTH_CLIENT_ID || 'REACT_APP_AUTH_CLIENT_ID',
+  auth_client_id: process.env.REACT_APP_AUTH_CLIENT_ID || 'AUTH0_CLIENT_ID',
   auth_callback_url: process.env.REACT_APP_AUTH_CALLBACK_URL || 'http://localhost:3000/authorization/callback',
-  login_required: process.env.REACT_APP_LOGIN_REQUIRED || false,
-  accepted_roles: ['admin'],
+  login_required: process.env.REACT_APP_LOGIN_REQUIRED || true,
+  accepted_roles: ['admin', 'unicef'],
   app_name: 'Magicbox Maps',
-  acceptable_percent: 60,
+  acceptable_percent: 80,
   map: {
-    lng: 23.684203,
-    lat: -3.078111,
+    lng:  -74.2973,
+    lat: 4.5709,
     zoom: 5
   }
 }
+
+// DRC
+// module.exports = {
+//   auth_client_id: process.env.REACT_APP_AUTH_CLIENT_ID || 'REACT_APP_AUTH_CLIENT_ID',
+//   auth_callback_url: process.env.REACT_APP_AUTH_CALLBACK_URL || 'http://localhost:3000/authorization/callback',
+//   login_required: process.env.REACT_APP_LOGIN_REQUIRED || false,
+//   accepted_roles: ['admin'],
+//   app_name: 'Magicbox Maps',
+//   acceptable_percent: 60,
+//   map: {
+//     lng: 23.684203,
+//     lat: -3.078111,
+//     zoom: 5
+//   }
+// }
