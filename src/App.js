9b598735ba9be6dcac6c2fc494f334df97b13eee
Thoa T <thoat@users.noreply.github.com>
2018-08-14 15:25:35 -0400

    Add UI functionalities for school connectivity

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ import Section from './components/section';
 import InputGroup from './components/input-group';
 import RadioGroup from './components/radio-group';
 import Legend from './components/legend';
+import ConnectivityChart from './components/connectivity-chart';
 
 // Helpers
 import apiConfig from './helpers/api-config';
+import countConnectivity from './helpers/count-connectivity';
 import helperMatrix from './helpers/helper-matrix';
 import helperGeojson from './helpers/helper-geojson';
 import helperMap from './helpers/helper-map';
@@ -50,6 +52,7 @@ class App extends Component {
 
     // Promises
     let shapesPromise  = fetch(apiConfig.shapes).then((resp) => resp.json())
+    let schoolsPromise = fetch(apiConfig.schools).then((resp) => resp.json())
     let mobilityMondayPromise =
       fetch(apiConfig.mobilityMon).then((resp) => resp.json())
     let mobilityTuesdayPromise =
@@ -125,10 +128,20 @@ class App extends Component {
     })
 
 
+    // Set data for schools when schools and map are available
+    Promise.all([schoolsPromise, mapLoadPromise]).then(([geojson, map]) => {
+      map.getSource('schools').setData(geojson)
+    })
+
     // Handle shapes data if any
     shapesPromise.then((myJson) => {
     })
 
+    // Handle school data if any
+    schoolsPromise.then((geojson) => {
+      this.setState({
+        connectivity_totals: countConnectivity(geojson.features)
+      })
     })
 
     // Make map respond when user zooms or moves it around
@@ -141,7 +154,12 @@ class App extends Component {
       });
     });
 
-    // Define map's properties when it is first loaded.
+    /*
+    Define map's properties when it's first loaded.
+    This includes:
+    - Adding 2 layers: 'regions' (for socio-econ metrics) & 'schools' (for school info & connectivity)
+    - Defining 'click' events for these 2 layers
+    */
     map.on('load', function(e) {
       map.addLayer({
         id: 'regions',
@@ -157,8 +175,25 @@ class App extends Component {
         layout: {
           visibility: 'none'
         },
+      });
+
+      map.addLayer({
+        id: 'schools',
+        type: 'circle',
+        // Add a GeoJSON source containing place coordinates and information.
+        source: {
+          type: 'geojson',
+          data: {
+            type: "FeatureCollection",
+            features: []
+          }
+        },
         paint: {
-          'fill-opacity': 0.5
+          'circle-radius': {
+            'base': 1.75,
+            'stops':[[12, 2], [22, 180]]
+          },
+          'circle-color': ['get', 'color']
         }
       });
 
@@ -220,6 +255,21 @@ class App extends Component {
         // )
       })
 
+      // Add click event to schools layer
+      map.on('click', 'schools', (e) => {
+        let coordinates = e.features[0].geometry.coordinates.slice()
+        let schoolProperties = e.features[0].properties
+
+        // output all properties besides color
+        let html = Object.keys(schoolProperties)
+          .filter((key) => key !== 'color')
+          .reduce((acc, key) => {
+          return acc + `<p><strong>${key}:</strong> ${schoolProperties[key]}</p>`
+        }, '')
+
+        new mapboxgl.Popup().setLngLat(coordinates).setHTML(html).addTo(map)
+      })
+
       // Change the cursor to a pointer
       map.on('mouseenter', 'regions', (e) => {
         map.getCanvas().style.cursor = 'pointer'
@@ -229,7 +279,25 @@ class App extends Component {
         map.getCanvas().style.cursor = ''
       })
 
+      map.on('mouseenter', 'schools', (e) => {
+        map.getCanvas().style.cursor = 'pointer'
+      })
+
+      map.on('mouseleave', 'schools', (e) => {
+        map.getCanvas().style.cursor = ''
+      })
+
     }.bind(this));
+  }
+
+  displayLayerHandler(e) {
+    // layer name should be stored in element's 'value' property
+    let layerName = e.target.getAttribute('value')
+    // will be 'visible' or 'none'
+    let currentState = e.target.checked ? 'visible' : 'none'
+
+    // Set layer visibility
+    this.state.map.setLayoutProperty(layerName, 'visibility', currentState)
   }
 
   changeRegionPaintPropertyHandler(e) {
@@ -371,8 +439,19 @@ class App extends Component {
             />
           </Section>
 
+          <Section title="School Capabilities">
+            <InputGroup type="checkbox" name="school" group={[
+              { value: 'schools',
+                label: 'Connectivity',
+                onChange: this.displayLayerHandler.bind(this),
+                defaultChecked: 'checked'
+              }
+            ]} onChange={(e) => {}} />
           </Section>
 
+          <Section title="Connectivity Details">
+            <ConnectivityChart totals={this.state.connectivity_totals}></ConnectivityChart>
+          </Section>
 
         </ControlPanel>
 
