ca637a20d4986bc9bf0f78dd9bdfda93ac7591b5
mikefab <mikefabrikant@gmail.com>
2018-08-15 12:00:20 -0400

    Bare bones dev

--- src/App.js
+++ src/App.js
@@ -1,184 +1,24 @@
 import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import config from './config'
 // Third-party React components
 import 'react-select/dist/react-select.css';
 import 'react-virtualized/styles.css';
 import 'react-virtualized-select/styles.css';
 import Select from 'react-virtualized-select';
-import createFilterOptions from 'react-select-fast-filter-options';
 
-// Custom React components
-import ControlPanel from './components/control-panel';
-import Section from './components/section';
-import InputGroup from './components/input-group';
-import Legend from './components/legend';
-import ConnectivityChart from './components/connectivity-chart';
-import ConnectivityChartMultiple from './components/connectivity-chart-multi';
-import setConnectivityColor from './helpers/set-connectivity-color';
-import RadioGroup from './components/radiogroup'
-
-// Helpers
-import {calculate_index} from './helpers/helper-index-scores';
-import apiConfig from './helpers/api-config';
-import countConnectivity from './helpers/count-connectivity';
-import countConnectivityMultiple from './helpers/count-connectivity-multi';
-// Main style
-import './css/App.css';
-// Map colors
-const mapColors = {
-  // higher color will be shown where indexes are 1
-  higher: '#0068EA',
-  // lower color will be shown where indexes are 0
-  lower: '#DCDCDC'
-}
-
-mapboxgl.accessToken = apiConfig.accessToken
 class App extends Component {
   constructor(props: Props) {
     super(props);
     this.state = {
-      map: {},
-      connectivityTotals: null,
-      options: [],
-      searchValue: '',
-      schools: {},
-      regions: {},
-      ...config.map
     };
   }
 
-  componentDidMount() {
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
-    });
-
-    this.setState({map});
-    // Promises
-    let schoolsPromise = fetch(apiConfig.schools).then((response) => response.json())
-    let mapLoadPromise = new Promise((resolve, reject) => {
-      map.on('load', (e) => {
-        resolve(map)
-      })
-
-      map.on('error', (e) => {
-        reject(map)
-      })
-    })
-
-    // Set data for regions when regions and map are available
-    Promise.all([mapLoadPromise, schoolsPromise]).then(([map, schools]) => {
-      map.addLayer({
-        id: 'schools',
-        type: 'circle',
-        // Add a GeoJSON source containing place coordinates and information.
-        source: {
-          type: 'geojson',
-          data: schools
-        },
-        paint: {
-           'circle-radius': {
-             'base': 1.75,
-             'stops':[[12, 2], [22, 180]]
-           },
-           'circle-color': ['get', 'color']
-         }
-       });
-    })
-
-    map.on('move', () => {
-      const { lng, lat } = map.getCenter();
-
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      });
-    });
-
-    map.on('load', function(e) {
-      // Add click event to schools layer
-      map.on('click', 'schools', (e) => {
-        let coordinates = e.features[0].geometry.coordinates.slice()
-        let schoolProperties = e.features[0].properties
-
-        // output all properties besides color
-        let html = Object.keys(schoolProperties)
-          .filter((key) => key !== 'color')
-          .reduce((acc, key) => {
-          return acc + `<p><strong>${key}:</strong> ${schoolProperties[key]}</p>`
-        }, '')
-
-        new mapboxgl.Popup().setLngLat(coordinates).setHTML(html).addTo(map)
-      })
-
-      // Change the cursor to a pointer
-      map.on('mouseenter', 'schools', (e) => {
-        map.getCanvas().style.cursor = 'pointer'
-      })
-
-      map.on('mouseleave', 'schools', (e) => {
-        map.getCanvas().style.cursor = ''
-      })
-    });
-  }
-
-  displayLayerHandler(e) {
-    // layer name should be stored in element's value property
-    let layerName = e.target.getAttribute('value')
-    // will be 'visible' or 'none'
-    let currentState = e.target.checked ? 'visible' : 'none'
-
-    // Set layer visibility
-    this.state.map.setLayoutProperty(layerName, 'visibility', currentState)
-  }
-
-  changeRegionPaintPropertyHandler(e) {
-    // Get all checked inputs for regions
-    let matches = document.querySelectorAll("input[name=region]:checked");
-
-    // Change layer visibility if there are matches
-    this.state.map.setLayoutProperty('regions', 'visibility', matches.length ? 'visible' : 'none')
-
-    if (!matches.length) {
-      // no region selected
-      return
-    }
-
-    // build the aggregation query
-    let atts_to_aggregate = Array.prototype.slice.call(matches).reduce((a,t) => {
-      a.push(['get', t.value])
-      return a
-    }, ['+'])
-
-    // Set new paint property to color the map
-    this.state.map.setPaintProperty(
-      'regions',
-      'fill-color',
-      // linear interpolation for colors going from lowerColor to higherColor accordingly to aggregation value
-      ['interpolate',
-        ['linear'],
-        ['/', atts_to_aggregate, atts_to_aggregate.length-1],
-        0, mapColors.lower,
-        1, mapColors.higher
-      ]
-    )
-  }
-
   render() {
-    let mainMap_class_name = config.login_required ? 'mainMap' : 'mainMap mainMap-noLogin'
     return (
       <div className="App">
-        <div>
-          <div ref={el => this.mapContainer = el} className={mainMap_class_name} />
-        </div>
-        <ControlPanel>
-        </ControlPanel>
-        <Legend from={mapColors.higher} to={mapColors.lower} steps={10} leftText="Most Risk" rightText="Least Risk" />
+        <p>
+         :0
+        </p>
       </div>
     );
   }
